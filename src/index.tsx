0efd298750575737db4c01cbac26da678e551e2c
Joshua Grosso <jgrosso256@gmail.com>
2019-03-02 13:39:37 -0800

    Fix my stupid

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const extensionId = 'extension-root';
-const extensionElement = document.getElementById(extensionId);
+let extensionElement = document.getElementById(extensionId);
 
 if (!extensionElement) {
-  const anchor = document.createElement('div');
-  anchor.id = extensionId;
-  document.body.insertBefore(anchor, document.body.childNodes[0]);
+  extensionElement = document.createElement('div');
+  extensionElement.id = extensionId;
+  document.body.insertBefore(extensionElement, document.body.childNodes[0]);
   ReactDOM.render(<App />, extensionElement);
 
   registerServiceWorker();
