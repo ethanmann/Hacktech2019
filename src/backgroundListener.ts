a9e18d674e97816e3fe1ae9be32c98349a80f887
Joshua Grosso <jgrosso256@gmail.com>
2019-03-02 21:19:51 -0800

    Fix overlay styling issues

--- src/backgroundListener.ts
+++ src/backgroundListener.ts
@@ -1,16 +1,16 @@
-const blockedList = ['games','twitter.com','reddit.com'];
+const blockedList = ['games', 'twitter.com', 'reddit.com'];
 
-function isBlockedURL(url: any){
-  if (typeof(url) !== "string"){
+const isBlockedURL = (url: string | null) => {
+  if (url === null) {
     return false;
   }
 
-  // https://www.typescriptlang.org/docs/handbook/iterators-and-generators.html
-  for (const blocked of blockedList){
-    if (url.indexOf(blocked) !== -1){
+  for (const blocked of blockedList) {
+    if (url.indexOf(blocked) !== -1) {
       return true;
     }
   }
+
   return false;
 }
 
@@ -21,9 +21,6 @@ const handleTabChange = (tabId: number) => {
         file: 'static/js/index.js',
         runAt: 'document_end'
       });
-      chrome.tabs.insertCSS(tabId, {
-        file: 'static/css/index.css'
-      });
     }
   });
 }
@@ -31,31 +28,25 @@ const handleTabChange = (tabId: number) => {
 let unproductiveTimer = 0;
 const TIMER_INTERVAL = 100;
 setInterval(() => {
-  unproductiveTimer += 0.001;
+  unproductiveTimer += 0.01;
 
-  chrome.runtime.sendMessage({
+  const message = {
     overlayScale: {
-      heightPercent: Math.min(unproductiveTimer / 100, 1),
-      widthPercent: Math.min(unproductiveTimer / 100, 1)
+      heightPercent: Math.min(unproductiveTimer / 100, 3/4),
+      widthPercent: Math.min(unproductiveTimer / 100, 3/4)
     },
     type: 'OVERLAY_SIZE_CHANGE'
-  });
+  };
 
+  chrome.runtime.sendMessage(message);
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-    chrome.tabs.sendMessage(tab.id!, {
-      overlayScale: {
-        heightPercent: unproductiveTimer,
-        widthPercent: unproductiveTimer
-      },
-      type: 'OVERLAY_SIZE_CHANGE'
-    });
+    chrome.tabs.sendMessage(tab.id!, message);
   });
 }, TIMER_INTERVAL);
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
    handleTabChange(activeInfo.tabId);
 })
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
     handleTabChange(tabId);
